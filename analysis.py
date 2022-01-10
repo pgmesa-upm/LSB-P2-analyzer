2962d12a264cea2c74110e23ac34259af679d16d
Pablo García Mesa <pablogmesa00@gmail.com>
2022-01-10 01:53:48 +0100

    Añadiendo test ANOVA

--- analysis.py
+++ analysis.py
@@ -7,6 +7,7 @@ from time import time as get_time
 from math import floor
 from pathlib import Path
 from shutil import copyfile
+from win32com.client import Dispatch
 
 # Dependencies
 import numpy as np
@@ -18,7 +19,7 @@ from openpyxl.workbook import Workbook
 from openpyxl.chart import ScatterChart, Reference, Series
 import openpyxl
 
-default_path = '../'; excel_template = 'excel_template.xlsx'
+default_path = '../'; stats_template = 'stats_template.xlsx'; anova_template = 'anova_template.xlsx'
 # ------------------------- Custom Settings -------------------------
 # -------------------------------------------------------------------
 custom_path = None
@@ -28,15 +29,19 @@ analysis_dir_name = '__analysis__'
 filt_dir_name = 'filtered_data'
 norm_dir_name = 'normalized_data'
 xcorr_dir_name = 'cross-correlations'
+anova_dir_name = 'ANOVA'
 # -- Excels
-excel_nt_pd_name = 'stats_vehiculos_PD.xlsx'
-excel_t_pd_name = 'stats_tratados_PD.xlsx'
-excel_nt_pi_name = 'stats_vehiculos_PI.xlsx'
-excel_t_pi_name = 'stats_tratados_PI.xlsx'
-excel_names = [excel_nt_pd_name, excel_t_pd_name, excel_nt_pi_name, excel_t_pi_name]
+stats_nt_pd_excelname = 'stats_vehiculos_PD.xlsx'
+stats_t_pd_excelname = 'stats_tratados_PD.xlsx'
+stats_nt_pi_excelname = 'stats_vehiculos_PI.xlsx'
+stats_t_pi_excelname = 'stats_tratados_PI.xlsx'
+anova_pd_hd_excelname = 'anova_PD_HD.xlsx'
+anova_pd_hi_excelname = 'anova_PD_HI.xlsx'
+anova_pi_hd_excelname = 'anova_PI_HD.xlsx'
+anova_pi_hi_excelname = 'anova_PI_HI.xlsx'
 # Nombre directorios ratones
-rigth_paw_dirs = ['pata-derecha_no-tratados', 'pata-derecha_tratados'] 
-left_paw_dirs = ['pata-izquierda_no-tratados', 'pata-izquierda_tratados']
+rigth_paw_nt_dir = 'pata-derecha_no-tratados'; rigth_paw_t_dir = 'pata-derecha_tratados'
+left_paw_nt_dir = 'pata-izquierda_no-tratados'; left_paw_t_dir = 'pata-izquierda_tratados'
 dir_basales = 'basales'; dir_72horas = '72-horas'; dir_2semanas = '2-semanas'
 # Stat headers
 mouse_header = "Raton"
@@ -51,10 +56,15 @@ if custom_path is not None:
     path = Path(custom_path).resolve()
 else:
     path = Path(default_path).resolve()
-
+    
+rigth_paw_dirs = [rigth_paw_nt_dir, rigth_paw_t_dir] 
+left_paw_dirs = [left_paw_nt_dir, left_paw_t_dir]
 paws_dir_names = rigth_paw_dirs + left_paw_dirs
 tgroups = [dir_basales, dir_72horas, dir_2semanas]
 stat_headers = [mouse_header, amplitud_header, latency_header, area_header, pearson_header]
+stats_excel_names = [stats_nt_pd_excelname, stats_t_pd_excelname, stats_nt_pi_excelname, stats_t_pi_excelname]
+anova_excelnames = [anova_pd_hd_excelname, anova_pd_hi_excelname, anova_pi_hd_excelname, anova_pi_hi_excelname]
+dep_variables = [amplitud_header, latency_header, area_header]
 
 reset_log = True
 
@@ -87,12 +97,16 @@ def main():
     if not os.path.exists(analysis_path/norm_dir_name): os.mkdir(analysis_path/norm_dir_name)
     if not os.path.exists(analysis_path/filt_dir_name): os.mkdir(analysis_path/filt_dir_name)
     if not os.path.exists(analysis_path/xcorr_dir_name): os.mkdir(analysis_path/xcorr_dir_name)
-    for excel in excel_names:
+    if not os.path.exists(analysis_path/anova_dir_name): os.mkdir(analysis_path/anova_dir_name)
+    for excel in stats_excel_names:
         if os.path.exists(analysis_path/excel): 
             os.remove(analysis_path/excel)
+    for excel in anova_excelnames:
+        if os.path.exists(analysis_path/anova_dir_name/excel): 
+            os.remove(analysis_path/anova_dir_name/excel)
     
     log(f" + Analysing '{path}'")
-    
+    letters = ['D', 'E', 'F', 'G', 'H', 'I', 'J', 'K']
     for ipaw, paw in enumerate(paws_dir_names):
         log(f" => {paw}")
         gcounter = 0
@@ -152,12 +166,11 @@ def main():
                 table.append(stats)
                 print(f"   - Actualizando excel...")
                 # Updateamos el excel
-                excel_path = analysis_path/excel_names[ipaw]
+                excel_path = analysis_path/stats_excel_names[ipaw]
                 if not os.path.exists(excel_path):
-                    copyfile(excel_template, excel_path)
+                    copyfile(stats_template, excel_path)
                 wb = openpyxl.load_workbook(excel_path)
                 ws = wb.active
-                letters = ['D', 'E', 'F', 'G', 'H', 'I', 'J', 'K']
                 offset = 4; counter = fcounter + gcounter
                 for i, stat in enumerate(stats):
                     if i == 0: continue
@@ -203,6 +216,49 @@ def main():
                 fcounter += 3
             gcounter += 1
             log(tabulate(table, headers='firstrow', tablefmt='fancy_grid', numalign='center'))
+    print("[%] Calculando estadísticos ANOVA...")
+    # Calculamos estadístico ANOVA (rellenamos datos en la plantilla)
+    anova_path = analysis_path/anova_dir_name
+    for i, name in enumerate(anova_excelnames):
+        anova_excel_path = anova_path/name
+        print(f"[%] Creando '{name}'")
+        if not os.path.exists(anova_excel_path):
+            copyfile(anova_template, anova_excel_path)
+        # Cargamos las hojas excel con las stats calculadas
+        index1 = 0; index2 = 1; h_offset = 0
+        if i >= 1:
+            index1 = 2; index2 = 3
+        nt_excel_name = stats_excel_names[index1]
+        t_excel_name = stats_excel_names[index2]
+        print(nt_excel_name, t_excel_name)
+        nt_excel_path = analysis_path/nt_excel_name
+        t_excel_path = analysis_path/t_excel_name
+        if i%2 != 0:
+            h_offset = 1
+        refresh_formulas(t_excel_path)
+        refresh_formulas(nt_excel_path)
+        nt_excel = openpyxl.load_workbook(nt_excel_path, data_only=True)
+        nt_ws = nt_excel['stats']
+        t_excel = openpyxl.load_workbook(t_excel_path, data_only=True)
+        t_ws = t_excel['stats']
+        start = 16; offset = 6
+        anova_wb = openpyxl.load_workbook(anova_excel_path)
+        # Guardamos los datos en el excel de ANOVA 
+        anova_letters = ['F', 'G', 'H']; anova_offset = 5; anova_start = 5
+        for j, dep_h in enumerate(dep_variables):
+            anova_ws = anova_wb[dep_h]
+            for k in range(3):
+                letter = letters[(j*2)+h_offset]
+                nt_mean = nt_ws[letter+str(start+k)].value
+                nt_variance = nt_ws[letter+str(start+k+offset)].value
+                t_mean = t_ws[letter+str(start+k)].value
+                t_variance = t_ws[letter+str(start+k+offset)].value
+                anova_letter = anova_letters[k]
+                anova_ws[anova_letter+str(anova_start)] = nt_mean
+                anova_ws[anova_letter+str(anova_start+1)] = t_mean
+                anova_ws[anova_letter+str(anova_start+anova_offset)] = nt_variance
+                anova_ws[anova_letter+str(anova_start+anova_offset+1)] = t_variance
+        anova_wb.save(anova_excel_path)
     print(f"[%] Se ha creado '/{analysis_dir_name}'")
     print("[%] Fin del analisis")
     
@@ -239,6 +295,14 @@ def save_data(t, hd, hi, dir_base:Path, fname:str, headers:list=None):
 def save_graph(dir_base:Path, fname:str):
     file_path = create_path(dir_base, fname)
     plt.savefig(file_path)
+    
+def refresh_formulas(excel_path):
+    xlApp = Dispatch("Excel.Application")
+    xlApp.Visible = False
+    xlBook = xlApp.Workbooks.Open(excel_path)
+    xlBook.RefreshAll()
+    xlBook.Save()
+    xlBook.Close()
 
 def filt(t, hd, hi, filt_time_ms=10):
     end_index_limit = None;
@@ -291,5 +355,5 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print("[!] Exit")
         exit(1)
-    except Exception as err:
-        print(f"[!] Unexpected Error: {err}")
+    # except Exception as err:
+    #     print(f"[!] Unexpected Error: {err}")
